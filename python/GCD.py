f7766d67871fee2f97b017b22a3b97d0eccae345
Joris Barkema <jorisbarkema@gmail.com>
2019-03-10 10:47:39 +0100

    remove python

--- python/GCD.py
+++
@@ -1,6 +0,0 @@
-class GCD:
-    def __init__(self, name):
-        self.name = name
-
-    def compute(self, p, q, debug = False):
-        raise("Trying to use abstract superclass")
--- python/Verifier.py
+++
@@ -1,6 +0,0 @@
-class Verifier:
-    def __init__(self, GCD):
-        self.GCD = GCD
-
-    def verify(self, values, result):
-        raise("Trying to use abstract superclass")
